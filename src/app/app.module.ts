ce4a57d29b107334f00787644306b9a59c8a720f
tran tien dat <kinomato005@gmail.com>
2019-05-19 15:24:13 +0700

    change something idk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -199,7 +199,7 @@ import { environment } from '../environments/environment';
     MatProgressBarModule,
     OrderModule,
     NgxPaginationModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
   ],
   exports: [
     MatAutocompleteModule,
